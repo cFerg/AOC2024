cc0c49ab44369e9f6b0e55ec3f9e5f2752a5e63b
Elite <mrcalebf@gmail.com>
2024-12-03 01:32:31 -0500

    Day3 - Brain fried remembering regexes

--- src/Day3.kt
+++ src/Day3.kt
@@ -1,14 +1,34 @@
 fun main(){
-    fun part1(): Int {return 0}
-    fun part2(): Int {return 0}
+    fun challenge(input: String): Pair<Long,Long> {
+        val mulSearch = Regex("""mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)""")
+
+        var multiply = true
+        var part1 = 0L
+        var part2 = 0L //Only add results when multiply is true
+
+        mulSearch.findAll(input).forEach{ m ->
+            when {
+                m.value.startsWith("don't()") -> multiply = false
+                m.value.startsWith("do()") -> multiply = true
+                m.value.startsWith("mul") -> {
+                    val (a,b) = m.destructured
+                    val mul = a.toInt() * b.toInt()
+                    part1 += mul
+                    if (multiply) part2 += mul
+                }
+            }
+        }
+
+        return Pair(part1,part2)
+    }
 
     //Testing lines - If any fails, it throws an error
-    val test = numberInputColumnGrouped(3, true, test = true)
-    check(part1() == 0)
-    check(part2() == 0)
+    val test = stringInput(3, test = true).toString()
+    check(challenge(test).first == 161L)
+    check(challenge(test).second == 48L)
 
     //Runs part 1 and 2
-    val input = numberInputColumnGrouped(3, true)
-    part1().println()
-    part2().println()
+    val input = stringInput(3).toString()
+    challenge(input).first.println()
+    challenge(input).second.println()
 }
